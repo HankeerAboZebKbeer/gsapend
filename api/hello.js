411011115d5536fb5136e66460795ad8e19c4c9c
HankeerSboZebKbeer <toska.atwork@gmail.com>
2025-07-13 14:09:29 +0300

    Clean verce.n

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Node.js serverless function!" });
+  res.status(200).json({ message: "Hello from Node.js on Vercel!" });
 }
